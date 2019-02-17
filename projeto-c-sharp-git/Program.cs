4f921bc7093817a24af70a85a8f7477386cbf4c4
Alonso-007 <alonso.afs@gmail.com>
2019-02-17 18:32:50 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ projeto-c-sharp-git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projeto_c_sharp_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+        }
+    }
+}
